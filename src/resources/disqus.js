dfbd8d178ccb2e2c3d07365355310b5550b6aee2
Hadrien Lanneau <hadrien@lanneau.me>
2016-10-06 21:53:58 +0200

    Refacto disqus service

--- src/resources/disqus.js
+++ src/resources/disqus.js
@@ -1,26 +1,40 @@
 import environment from '../environment';
 
 export class Disqus {
+  _instances = {};
+
   constructor() {
-    this.getInstance();
+    this.getInstance('embed');
   }
 
   async getInstance(type) {
-    return new Promise(resolve => {
-      if (this._instance) {
-        resolve(this._instance);
-      } else {
-        const d = document;
-        const s = d.createElement('script');
-        s.src = `//${environment.disqus}.disqus.com/${type}.js`;
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-        s.onload = () => {
-          this._instance = window.DISQUS;
-          resolve(this._instance);
-        };
-      }
-    });
+    if (!this._instances[type]) {
+      this._instances[type] = new Promise((resolve, reject) => {
+        if (this._instances[type]) {
+          resolve(this._instance[type]);
+        } else {
+          const d = document;
+          const s = d.createElement('script');
+          s.src = `//${environment.disqus}.disqus.com/${type}.js`;
+          s.setAttribute('data-timestamp', +new Date());
+          (d.head || d.body).appendChild(s);
+          s.onload = () => {
+            switch (type) {
+            case 'embed':
+              this._instances[type] = window.DISQUS;
+              break;
+            case 'count':
+              this._instances[type] = window.DISQUSWIDGETS;
+              break;
+            default:
+              reject(new Error('Wrong type'));
+            }
+            resolve(this._instances[type]);
+          };
+        }
+      });
+    }
+    return this._instances[type];
   }
 
   async reset() {
@@ -31,7 +45,7 @@ export class Disqus {
   }
 
   async count() {
-    await this.getInstance('count');
-    DISQUSWIDGETS.getCount({ reset: true });
+    const disqus = await this.getInstance('count');
+    disqus.getCount({ reset: true });
   }
 }
